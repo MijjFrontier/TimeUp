11e3c1b36797558cf25bdc33fd416d7f01cc8392
Mijj Herrera <guillenherrera.MFGH@gmail.com>
2025-08-10 01:45:12 +0000

    The `npm install` command failed in my project. Analyze the following er

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,6 +9,7 @@ export const metadata: Metadata = {
   icons: {
     icon: '/icon.svg',
   },
+  manifest: "/manifest.json",
 };
 
 export default function RootLayout({
@@ -22,6 +23,7 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=PT+Sans:wght@400;700&display=swap" rel="stylesheet" />
+        <meta name="theme-color" content="#F0F4F8" />
       </head>
       <body className="font-body antialiased">
         <AppShell>
